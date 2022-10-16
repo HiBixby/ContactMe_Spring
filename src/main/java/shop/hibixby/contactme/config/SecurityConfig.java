048fea98b832b6a767925a29c4401053f086d9d9
HiBixby <201910789@sangmyung.kr>
2022-10-17 08:39:06 +0900

    🔧 Add a security config - permit all request

---
+++ src/main/java/shop/hibixby/contactme/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package shop.hibixby.contactme.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
+    }
+
+}
