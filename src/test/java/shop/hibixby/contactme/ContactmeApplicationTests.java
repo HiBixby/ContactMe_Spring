c1e4ecaa5bcd11d99c73ee04e2730f1ef9db1e55
HiBixby <201910789@sangmyung.kr>
2022-10-16 20:32:17 +0900

    🎉 Begin a project

---
+++ src/test/java/shop/hibixby/contactme/ContactmeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.hibixby.contactme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
